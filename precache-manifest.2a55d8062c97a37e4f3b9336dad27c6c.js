ffa21f6dfbd641346f7d748be408066e75a256df
Donais <kingofthedonais@gmail.com>
2020-12-17 18:25:59 +0500

    Updates

---
+++ precache-manifest.2a55d8062c97a37e4f3b9336dad27c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ddfde3c954d8aabedb9ef417a31fb2",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "84f836abb1b979984194",
+    "url": "/Todo-list/static/css/main.2514f1ca.chunk.css"
+  },
+  {
+    "revision": "f7339d54af16e82d8f4e",
+    "url": "/Todo-list/static/js/2.802c7d84.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todo-list/static/js/2.802c7d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f836abb1b979984194",
+    "url": "/Todo-list/static/js/main.69f69778.chunk.js"
+  },
+  {
+    "revision": "5949f8058790574992ac",
+    "url": "/Todo-list/static/js/runtime-main.d2fc529c.js"
+  }
+]);
